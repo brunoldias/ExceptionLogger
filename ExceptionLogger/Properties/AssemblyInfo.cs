47903e13428cb50e3aee535da0e6a57a4a3eda74
Bruno Lourenço <brunolourencodias@hotmail.com.br>
2022-04-25 01:11:32 -0300

    nuget spec

--- ExceptionLogger/Properties/AssemblyInfo.cs
+++ ExceptionLogger/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ExceptionLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BLite Software")]
 [assembly: AssemblyProduct("ExceptionLogger")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
